cd6af3fa78ccbcf62b269c88aa9269c34826bb0a
garizola <99741841+garizola@users.noreply.github.com>
2024-10-22 22:30:57 -0500

    height change

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,7 +7,7 @@ function Todo({ todo_name, todo_id, id, deleteTodo }) {
     deleteTodo(id);
   };
   return (
-    <div className="h-auto max-h-[300px] flex-col flex align-middle justify-center sm:w-[30%] max-w-[100%] min-w-[200px] bg-off-white pb-4 p-2 border-b-2 border-black">
+    <div className="h-auto max-h-[300px] md:h-[300px] flex-col flex align-middle justify-center sm:w-[30%] w-[100%] min-w-[200px] bg-off-white pb-4 p-2 border-b-2 border-black">
       <div className="flex w-full justify-between pb-2">
         <p className="font-bold">{todo_id}</p>
         <span
